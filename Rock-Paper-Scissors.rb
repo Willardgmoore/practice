06ae18750e3d928eaf907cea564d3ca85588ac55
willardgmoore <will@thefixitgeek.com>
2015-06-04 17:08:39 -0600

    Syntax, Comments
    Improved code readability by:
    Spacing
    Comment

--- Rock-Paper-Scissors.rb
+++ Rock-Paper-Scissors.rb
@@ -6,13 +6,16 @@ play_game = "Y"
 $wins = 0
 $losses = 0
 $draw = 0
+
 def game
   rps_array = ["rock","paper","scissors"]
   player_1 = ""
   player_2 = (rps_array.sample).to_s
+
   until rps_array.include?(player_1)
     puts "Make your selection: rock, paper or scissors"
     player_1 = gets.chomp.downcase
+
     if player_1 == "r"		## quick letter mode
       player_1 = "rock"
     elsif player_1 == "p"
@@ -22,7 +25,8 @@ def game
     end
   end
   puts player_1 +" vs "+ player_2
-  if player_1 == player_2
+
+  if player_1 == player_2     # Win/Lose/Draw Algorythm
     puts "Draw"
     $draw+=1
   elsif (player_1 == "rock" and player_2 == "scissors") ||
@@ -35,12 +39,15 @@ def game
     $losses+=1
   end
 end
+
 while play_game == "Y"
   game
 
   puts "Wins: #{$wins} Losses:#{$losses} Draw:#{$draw}"
   puts "Would you like to play again? Y/N"
   play_game = gets.chomp.upcase
+end
+
   if play_game == "N"
     puts "See you later!"
     puts "Final Score: #{$wins} out of #{($wins + $losses)}, #{($wins / ($wins + $losses))}"
